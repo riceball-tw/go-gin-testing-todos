126e665ed975bfa93a0bd9cdc7632cc150f23a54
riceball <42161586+riceball-tw@users.noreply.github.com>
2026-05-06 11:45:17 +0800

    feat: Only use lumberjack build log files in prod

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,25 +2,34 @@ package logger
 
 import (
 	"log/slog"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/natefinch/lumberjack"
 )
 
 var Log *slog.Logger
 
 func InitLogger() {
-	handler := slog.NewJSONHandler(&lumberjack.Logger{
-		Filename:   "logs/app.log",
-		MaxSize:    10,
-		MaxBackups: 5,
-		MaxAge:     30,
-		Compress:   true,
-	}, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	})
+	var handler slog.Handler
+
+	if gin.Mode() == gin.DebugMode {
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})
+	} else {
+		handler = slog.NewJSONHandler(&lumberjack.Logger{
+			Filename:   "logs/app.log",
+			MaxSize:    10,
+			MaxBackups: 5,
+			MaxAge:     30,
+			Compress:   true,
+		}, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		})
+	}
 
 	Log = slog.New(handler.WithAttrs([]slog.Attr{
-		// Add global characteristics to all logs from this logger
 		slog.String("service", "go-gin-testing-todos"),
 	}))
 
